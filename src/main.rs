79043a5551041cd571d9c4c097ce9b1e3e66c85c
arleyribeiro <arley.sribeiro@gmail.com>
2020-09-01 21:52:35 -0300

    Basic format println!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod print;
+
 fn main() {
-    println!("Hello, world!");
+    print::run();
+
+    // Basic Formatting
+    println!("{} is from {}", "Brad", "Mass");
+
+    // Positional Arguments
+    println!("{0} is from {1} and {0} likes to {2}", "Brad", "Mass", "Code");
+
+    // Named Arguments
+    println!("{name} likes to play {activity}", name = "Jonh", activity="Baseball");
 }
