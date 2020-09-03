8ee0468f8339f5fed0f9e0144cc9c962f4da2531
arleyribeiro <arley.sribeiro@gmail.com>
2020-09-03 19:44:38 -0300

    Fix folder course

---
+++ rust-crash-course/target/debug/deps/rust-c31a446897d92421.d
@@ -0,0 +1,6 @@
+/home/arleyribeiro/GitHub/rust/target/debug/deps/rust-c31a446897d92421: src/main.rs src/strings.rs
+
+/home/arleyribeiro/GitHub/rust/target/debug/deps/rust-c31a446897d92421.d: src/main.rs src/strings.rs
+
+src/main.rs:
+src/strings.rs:
---
+++ rust-crash-course/target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/arleyribeiro/GitHub/rust/target/debug/rust: /home/arleyribeiro/GitHub/rust/src/main.rs /home/arleyribeiro/GitHub/rust/src/strings.rs
