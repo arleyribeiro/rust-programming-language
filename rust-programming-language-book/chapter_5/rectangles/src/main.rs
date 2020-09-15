74f501405e0c19d5e0bc65bdd7f5ddf229718866
arleyribeiro <arley.sribeiro@gmail.com>
2020-09-15 11:36:10 -0300

    https://doc.rust-lang.org/book/ch05-03-method-syntax.html

--- rust-programming-language-book/chapter_5/rectangles/src/main.rs
+++ rust-programming-language-book/chapter_5/rectangles/src/main.rs
@@ -6,9 +6,9 @@ fn main() {
     let width1: u32 = 30;
     let height1: u32 = 50;
 
-    let rect1 = (30, 50);
+    let rect_tuple = (30, 50);
 
-    let rect2 = rectangle::create(width1, height1);
+    let rect = rectangle::create(width1, height1);
 
     // Calculating the area of a rectangle specified by separate width and height variables
     println!(
@@ -19,14 +19,25 @@ fn main() {
     // Specifying the width and height of the rectangle with a tuple
     println!(
         "The area of the rectangle is {} square pixels.",
-        area_tuples::area(rect1)
+        area_tuples::area(rect_tuple)
     );
 
     // Defining a Rectangle struct
     println!(
         "The area of the rectangle is {} square pixels.",
-        rectangle::area(&rect2)
+        rectangle::area(&rect)
     );
-    println!("rect2 is {:?}", rect2);
-    println!("rect2 is {:#?}", rect2);
+    println!("rect is {:?}", rect);
+    println!("rect is {:#?}", rect);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
+
+    let rect1 = rectangle::create(30, 50);
+    let rect2 = rectangle::create(10, 40);
+    let rect3 = rectangle::create(60, 45);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
--- rust-programming-language-book/chapter_5/rectangles/src/rectangle.rs
+++ rust-programming-language-book/chapter_5/rectangles/src/rectangle.rs
@@ -4,6 +4,22 @@ pub struct Rectangle {
   height: u32,
 }
 
+// Defining an area method on the Rectangle struct
+impl Rectangle {
+  pub fn area(&self) -> u32 {
+    self.width * self.height
+  }
+  pub fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
+  pub fn square(size: u32) -> Rectangle {
+    Rectangle {
+      width: size,
+      height: size,
+    }
+  }
+}
+
 pub fn area(rectangle: &Rectangle) -> u32 {
   rectangle.width * rectangle.height
 }
